555451f80f980ae93b23196a5a6a773d446815af
Muskan Jain <jainmuskaan698@gmail.com>
2025-07-30 23:51:57 +0530

    Adding test cases for add function

---
+++ src/com/example/Calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.Calculator;
+
+public class Calculator {
+
+    public int add(String numbers){
+        return 33333;
+    }
+}
---
+++ src/com/example/Calculator/CalculatorTest.java
@@ -0,0 +1,26 @@
+package com.example.Calculator;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void add_returnsZero_whenEmptyString(){
+        assertEquals(0,calculator.add(""));
+    }
+
+    @Test
+    public void add_returnsVal_whenSingleNumber(){
+        assertEquals(1,calculator.add("1"));
+    }
+
+    @Test
+    public void add_returnsVal_whenTwoNumbers(){
+        assertEquals(6,"1,5");
+    }
+
+}
