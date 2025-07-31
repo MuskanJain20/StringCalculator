525b0416879e3e9da89c6ecc415b1db61f9a367d
Muskan Jain <jainmuskaan698@gmail.com>
2025-07-31 19:29:37 +0530

    Adding code for the test cases

--- src/com/example/Calculator/Calculator.java
+++ src/com/example/Calculator/Calculator.java
@@ -3,6 +3,15 @@ package com.example.Calculator;
 public class Calculator {
 
     public int add(String numbers){
-        return 33333;
+        if(numbers.isEmpty()){
+            return 0;           // returnsZero_whenEmptyString
+        }
+
+        String[] parts = numbers.split(",");
+        int sum = 0;
+        for(String part : parts){            // whenTwoNumbers
+            sum+= Integer.parseInt(part);
+        }
+        return sum;
     }
 }
