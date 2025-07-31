da4b3db2424937ae7ff687c7af1d9acffa0561e0
Muskan Jain <jainmuskaan698@gmail.com>
2025-07-31 19:58:39 +0530

    Adding code for new line as delimiter

--- src/com/example/Calculator/Calculator.java
+++ src/com/example/Calculator/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
             return 0;           // returnsZero_whenEmptyString
         }
 
-        String[] parts = numbers.split(",");
+        String[] parts = numbers.split(",|\\n");
         int sum = 0;
         for(String part : parts){            // whenTwoNumbers
             sum+= Integer.parseInt(part);
